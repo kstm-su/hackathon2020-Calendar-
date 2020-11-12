c4844f64e42210531c6697c0803719e159afb5d6
17t2037h <17t2037h@shinshu-u.ac.jp>
2020-11-13 04:01:27 +0900

    Changed to be env adaptive.

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # hackathon2020-Calendar-
 
+##必須手順  
+.envファイルの
+LINUX_MYSQL_UID  
+LINUX_MYSQL_GID  
+を、下記コマンドの結果の値に書き換えてください
+id -u $USER  
+id -g $USER  
+
 ##docker, docker-composeによるテスト用コマンド  
 コンテナのビルド : docker-compose build  
 コンテナの起動 : docker-compose up -d  
 コンテナの生死確認 : docker-compose ps  
-コンテナの終了&削除 : docker-compose down 
+コンテナの終了&削除 : docker-compose down
 
 ##コンテナ名  
-ウェブアプリケーションサーバ : hackerthon2020_web_1  
-データベースサーバ : hackerthon2020_mysql_1  
+ウェブアプリケーションサーバ : web  
+データベースサーバ : mysql  
---
+++ html/upload.php
@@ -0,0 +1,50 @@
+<?php
+
+//echo <<<_END
+//<html>
+//  <head>
+//    <title>フォームテスト</title>
+//  </head>
+//  <body>
+//  <form method="post" action="upload.php">
+//   what's your name?
+//   <input type="text" name="name" />
+//   <input type="submit" />
+//  </form>
+//  </body>
+//</html>
+//_END;
+
+
+ require_once 'login.php';
+ try{
+   $pdo = new PDO($db_host,$db_username,$db_password);
+   $sql = "select * from city;";
+   $res = $pdo->query($sql);
+   // echo "$value";
+   foreach( $res as $value ) {
+     echo "$value[Name]<br>";
+   }
+ } catch (PDOException $e) {
+   exit('データベースに接続できませんでした。' . $e->getMessage());
+   die();
+ }
+ $pdo = null;
+
+// echo <<<_END
+// <html><head>
+// <title>PHP Form Upload</title></head>
+// </head>
+// <body>
+// <form method='post' action='upload.php' enctype='multipart/form-data'>
+// Select File: <input type='file' name='filename' size='10'/>
+// <input type='submit' value='Upload'/>
+// </form>
+// _END;
+// if($_FILES){
+//   $name = $_FILES['filename']['name'];
+//   move_uploaded_file($_FILES['filename']['tmp_name'], $name);
+//   echo "Uploaded image '$name' <br /><img src='$name' />";
+// }
+// echo "</body></html>"
+?>
