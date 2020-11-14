2c69459c831f6fe234d1e12a5ab7ff69c5f2a67c
passerby-1 <ps1@passerby1.dev>
2020-11-15 01:41:31 +0900

    moditied colour in test1.sql

--- MySQL/test1.sql
+++ MySQL/test1.sql
@@ -15,6 +15,7 @@ CREATE TABLE Teams (
     teamid TEXT NOT NULL,
     teamname TEXT NOT NULL,
     ismyself INTEGER
+    color TEXT NOT NULL
 );
 
 CREATE TABLE Events (
@@ -23,7 +24,6 @@ CREATE TABLE Events (
     eventid TEXT NOT NULL,
     eventname TEXT, starttime DATETIME,
     endtime DATETIME, priority INTEGER NOT NULL,
-    color TEXT,
     memo TEXT,
     istodo INTEGER DEFAULT 0,
     istimetable INTEGER DEFAULT 0
@@ -33,11 +33,11 @@ CREATE TABLE Events (
 INSERT INTO Users (userid, username, teamid) VALUES ('001','passerby1','clubroom-imbibe-compost');
 INSERT INTO Users (userid, username, teamid) VALUES ('002','passerby2','quondam-chimney-wont');
 INSERT INTO Users (userid, username, teamid) VALUES ('003','passerby3','silage-croon-lavish');
-INSERT INTO Teams (teamid, teamname, ismyself) VALUES('clubroom-imbibe-compost','default','1');
-INSERT INTO Teams (teamid, teamname, ismyself) VALUES('quondam-chimney-wont','default','1');
-INSERT INTO Teams (teamid, teamname, ismyself) VALUES('silage-croon-lavish','default','1');
-INSERT INTO Teams (teamid, teamname, ismyself) VALUES('militia-manteau-twill','kstm','0');
+INSERT INTO Teams (teamid, teamname, ismyself) VALUES('clubroom-imbibe-compost','default','1', '#4169e1');
+INSERT INTO Teams (teamid, teamname, ismyself) VALUES('quondam-chimney-wont','default','1', '#ffa500');
+INSERT INTO Teams (teamid, teamname, ismyself) VALUES('silage-croon-lavish','default','1', '#7cfc00');
+INSERT INTO Teams (teamid, teamname, ismyself) VALUES('militia-manteau-twill','kstm','0', '#fa8072');
 INSERT INTO Users (userid, username, teamid) VALUES ('001','passerby1','militia-manteau-twill');
-INSERT INTO Events (teamid, eventid, eventname, starttime, endtime, color, priority, memo, istodo, istimetable) VALUES('militia-manteau-twill','aaa', 'kstmLT', '2020-11-14 20:00:00', '2020-11-14 21:00:00', 'f0f8ff', '1', 'MEMOTEST', '0', '0');
-INSERT INTO Events (teamid, eventid, eventname, starttime, endtime, color, priority, memo, istodo, istimetable) VALUES('clubroom-imbibe-compost','bbb', 'KADAI-1', '2020-11-14 21:00:00', '2020-11-14 21:00:00', 'ff6347', '0', 'MEMO-KADAI', '1', '0');
-INSERT INTO Events (teamid, eventid, eventname, starttime, endtime, color, priority, memo, istodo, istimetable) VALUES('clubroom-imbibe-compost','ccc', 'KOUGI-1', '2020-11-18 10:40:00', '2020-11-18 12:10:00', '32cd32', '0', 'MEMO-KOUGI', '0', '1');
+INSERT INTO Events (teamid, eventid, eventname, starttime, endtime, priority, memo, istodo, istimetable) VALUES('militia-manteau-twill','aaa', 'kstmLT', '2020-11-14 20:00:00', '2020-11-14 21:00:00', '1', 'MEMOTEST', '0', '0');
+INSERT INTO Events (teamid, eventid, eventname, starttime, endtime, priority, memo, istodo, istimetable) VALUES('clubroom-imbibe-compost','bbb', 'KADAI-1', '2020-11-14 21:00:00', '2020-11-14 21:00:00', '0', 'MEMO-KADAI', '1', '0');
+INSERT INTO Events (teamid, eventid, eventname, starttime, endtime, priority, memo, istodo, istimetable) VALUES('clubroom-imbibe-compost','ccc', 'KOUGI-1', '2020-11-18 10:40:00', '2020-11-18 12:10:00', '0', 'MEMO-KOUGI', '0', '1');
