03a1360861f8f3389765f166b6b8bbe6c5599a54
17t2037h <17t2037h@shinshu-u.ac.jp>
2020-11-15 02:11:19 +0900

    resolved sql error.

--- MySQL/test1.sql
+++ MySQL/test1.sql
@@ -14,7 +14,7 @@ CREATE TABLE Teams (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
     teamid TEXT NOT NULL,
     teamname TEXT NOT NULL,
-    ismyself INTEGER
+    ismyself INTEGER,
     color TEXT NOT NULL
 );
 
@@ -33,10 +33,10 @@ CREATE TABLE Events (
 INSERT INTO Users (userid, username, teamid) VALUES ('001','passerby1','clubroom-imbibe-compost');
 INSERT INTO Users (userid, username, teamid) VALUES ('002','passerby2','quondam-chimney-wont');
 INSERT INTO Users (userid, username, teamid) VALUES ('003','passerby3','silage-croon-lavish');
-INSERT INTO Teams (teamid, teamname, ismyself) VALUES('clubroom-imbibe-compost','default','1', '4169e1');
-INSERT INTO Teams (teamid, teamname, ismyself) VALUES('quondam-chimney-wont','default','1', 'ffa500');
-INSERT INTO Teams (teamid, teamname, ismyself) VALUES('silage-croon-lavish','default','1', '7cfc00');
-INSERT INTO Teams (teamid, teamname, ismyself) VALUES('militia-manteau-twill','kstm','0', 'fa8072');
+INSERT INTO Teams (teamid, teamname, ismyself, color) VALUES('clubroom-imbibe-compost','default','1', '4169e1');
+INSERT INTO Teams (teamid, teamname, ismyself, color) VALUES('quondam-chimney-wont','default','1', 'ffa500');
+INSERT INTO Teams (teamid, teamname, ismyself, color) VALUES('silage-croon-lavish','default','1', '7cfc00');
+INSERT INTO Teams (teamid, teamname, ismyself, color) VALUES('militia-manteau-twill','kstm','0', 'fa8072');
 INSERT INTO Users (userid, username, teamid) VALUES ('001','passerby1','militia-manteau-twill');
 INSERT INTO Events (teamid, eventid, eventname, starttime, endtime, priority, memo, istodo, istimetable) VALUES('militia-manteau-twill','aaa', 'kstmLT', '2020-11-14 20:00:00', '2020-11-14 21:00:00', '1', 'MEMOTEST', '0', '0');
 INSERT INTO Events (teamid, eventid, eventname, starttime, endtime, priority, memo, istodo, istimetable) VALUES('clubroom-imbibe-compost','bbb', 'KADAI-1', '2020-11-14 21:00:00', '2020-11-14 21:00:00', '0', 'MEMO-KADAI', '1', '0');
